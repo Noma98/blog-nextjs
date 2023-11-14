207ab739844d2c62003c84e26dd0d4c528c22d83
Noma <wiostz98kr@gmail.com>
2023-11-14 21:04:31 +0900

    Feat: Implement "you may like" section into carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image';
 
 import Profile from '@public/images/home/profile.jpg';
 import { getPosts } from '@/service/posts';
+import CarouselPost from '@/components/CarouselPost';
 
 export default async function Home() {
   const posts = await getPosts();
@@ -37,43 +38,21 @@ export default async function Home() {
                   alt='post_thumbnail'
                   width={400}
                   height={300}
+                  className='w-full'
                 />
                 <span className='text-xs align-bottom mt-2 ml-auto mr-2 text-gray-400'>
                   {post.date}
                 </span>
                 <h3 className='font-semibold mt-2'>{post.title}</h3>
                 <p className='text-sm'>{post.description}</p>
-                <span className='rounded-md px-2 py-1 text-xs mt-4 bg-yellow-400 '>
+                <span className='rounded-md px-2 py-1 text-xs mt-4 bg-opacity-30 bg-blue-400 '>
                   {post.category}
                 </span>
               </div>
             ))}
         </div>
         <h2 className='font-bold text-xl mt-8'>You may like</h2>
-        <div className='my-4 flex flex-col gap-4 overflow-scroll p-2 sm:flex-row'>
-          {posts.map((post, idx) => (
-            <div
-              key={idx}
-              className='flex flex-col items-center shadow-md shadow-slate-300 rounded p-4'
-            >
-              <Image
-                src={`/images/home/${post.path}.png`}
-                alt='post_thumbnail'
-                width={300}
-                height={200}
-                className='max-w-none'
-              />
-              <span className='text-xs align-bottom mt-2 ml-auto mr-2 text-gray-400'>
-                {post.date}
-              </span>
-              <h3 className='font-semibold mt-2'>{post.title}</h3>
-              <p className='text-sm'>{post.description}</p>
-              <span className='rounded-md px-2 py-1 text-xs mt-4 bg-yellow-400 '>
-                {post.category}
-              </span>
-            </div>
-          ))}
-        </div>
+        <CarouselPost posts={posts} />
       </div>
     </main>
   );
---
+++ src/components/CarouselPost.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import React from 'react';
+import Image from 'next/image';
+
+import { Post } from '@/service/posts';
+
+type Props = {
+  posts: Post[];
+};
+function CarouselPost({ posts }: Props) {
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1,
+    },
+  };
+  return (
+    <Carousel
+      responsive={responsive}
+      infinite
+      centerMode
+      className='mb-10 min-h-100 p-4'
+    >
+      {posts.map((post, idx) => (
+        <div
+          key={idx}
+          className='flex flex-col items-center shadow-md shadow-slate-300 rounded p-4 mr-4 h-full'
+        >
+          <Image
+            src={`/images/home/${post.path}.png`}
+            alt='post_thumbnail'
+            width={300}
+            height={200}
+            className='w-full'
+          />
+          <span className='text-xs align-bottom mt-2 ml-auto mr-2 text-gray-400'>
+            {post.date}
+          </span>
+          <h3 className='font-semibold mt-2 font-sm'>{post.title}</h3>
+          <p className='text-sm'>{post.description}</p>
+          <span className='rounded-md px-2 py-1 text-xs mt-4 bg-yellow-200'>
+            {post.category}
+          </span>
+        </div>
+      ))}
+    </Carousel>
+  );
+}
+
+export default CarouselPost;
