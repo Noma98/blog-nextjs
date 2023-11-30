39acc90d3e97453ac75149a896d52f3640836f53
Noma <wiostz98kr@gmail.com>
2023-11-30 19:10:33 +0900

    Feat: Separate featured post from "/" page into client component
    - To move to another page when I click post item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Profile from '@public/images/home/profile.jpg';
 import { getPosts } from '@/service/posts';
 import CarouselPost from '@/components/CarouselPost';
+import FeaturedPost from '@/components/FeaturedPost';
 
 export default async function Home() {
   const posts = await getPosts();
@@ -29,26 +30,7 @@ export default async function Home() {
           {posts
             .filter((post) => post.featured)
             .map((post, idx) => (
-              <div
-                key={idx}
-                className='flex flex-col items-center shadow-md shadow-slate-200 rounded p-4'
-              >
-                <Image
-                  src={`/images/home/${post.path}.png`}
-                  alt='post_thumbnail'
-                  width={400}
-                  height={300}
-                  className='w-full'
-                />
-                <span className='text-xs align-bottom mt-2 ml-auto mr-2 text-gray-400'>
-                  {post.date}
-                </span>
-                <h3 className='font-semibold mt-2'>{post.title}</h3>
-                <p className='text-sm'>{post.description}</p>
-                <span className='rounded-md px-2 py-1 text-xs mt-4 bg-opacity-30 bg-blue-400 '>
-                  {post.category}
-                </span>
-              </div>
+              <FeaturedPost idx={idx} {...post} />
             ))}
         </div>
         <h2 className='font-bold text-xl mt-8'>You may like</h2>
---
+++ src/components/FeaturedPost.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+type Props = {
+  idx: number;
+  path: string;
+  date: string;
+  title: string;
+  description: string;
+  category: string;
+};
+function FeaturedPost({
+  idx,
+  path,
+  date,
+  title,
+  description,
+  category,
+}: Props) {
+  const router = useRouter();
+
+  return (
+    <button
+      key={idx}
+      onClick={() => router.push(`/posts/${path}`)}
+      className='flex flex-col items-center shadow-md shadow-slate-200 rounded p-4 hover:opacity-80'
+    >
+      <Image
+        src={`/images/home/${path}.png`}
+        alt='post_thumbnail'
+        width={400}
+        height={300}
+        className='w-full'
+      />
+      <span className='text-xs align-bottom mt-2 ml-auto mr-2 text-gray-400'>
+        {date}
+      </span>
+      <h3 className='font-semibold mt-2'>{title}</h3>
+      <p className='text-sm'>{description}</p>
+      <span className='rounded-md px-2 py-1 text-xs mt-4 bg-opacity-30 bg-blue-400 '>
+        {category}
+      </span>
+    </button>
+  );
+}
+
+export default FeaturedPost;
