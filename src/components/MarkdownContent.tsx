5f75b09bfd569ace120db0f07705e9cb1fa112d2
Noma <wiostz98kr@gmail.com>
2023-12-05 20:13:19 +0900

    Mod: Modify markdown to make it prettier and more efficiently rendered
    - Remove the default max-width setting for react-markdown
    - Support GFM(GitHub Flavored Markdown) using remark-gfm plugin
    - Change react-syntax-highlighter style
    - Specify an img to be rendered as an <Image> tag of nextjs

--- src/components/MarkdownContent.tsx
+++ src/components/MarkdownContent.tsx
@@ -1,8 +1,10 @@
 'use client';
 import React from 'react';
 import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import Image from 'next/image';
 
 type Props = {
   data: string;
@@ -10,7 +12,8 @@ type Props = {
 function MarkdownContent({ data }: Props) {
   return (
     <Markdown
-      className='prose'
+      className='prose lg:prose-xl max-w-none'
+      remarkPlugins={[remarkGfm]}
       components={{
         code({ children, className, node, ...rest }: any) {
           const match = /language-(\w+)/.exec(className || '');
@@ -19,7 +22,7 @@ function MarkdownContent({ data }: Props) {
               {...rest}
               PreTag='div'
               language={match[1]}
-              style={dark}
+              style={materialDark}
             >
               {String(children).replace(/\n$/, '')}
             </SyntaxHighlighter>
@@ -29,6 +32,15 @@ function MarkdownContent({ data }: Props) {
             </code>
           );
         },
+        img: (image) => (
+          <Image
+            className='w-full max-h-60 object-cover'
+            src={image.src || ''}
+            alt={image.alt || ''}
+            width={500}
+            height={350}
+          />
+        ),
       }}
     >
       {data}
