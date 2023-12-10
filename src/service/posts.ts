d80f755c28b70c2a3c8ab24997079917355229e4
Noma <wiostz98kr@gmail.com>
2023-12-10 22:49:22 +0900

    Feat: Example of performance optimization with cache

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 import { readFile } from 'fs/promises';
+import { cache } from 'react';
 
 export type Post = {
   title: string;
@@ -15,12 +16,16 @@ export type PostData = Post & {
   prev: Post | null;
 };
 
-export async function getAllPosts(): Promise<Post[]> {
+//fetch를 사용하지 않고 요청할 때(DB 접근 등..) 중복을 제거하려면 react의 cache를 사용하면 됨.
+//한 페이지의 렌더링 사이클에 한해서 동일 인자에 대해 캐싱이 됨
+//SSR할 때만 유의미한 성능 개선이 존재함.
+export const getAllPosts = cache(async () => {
   const filePath = path.join(process.cwd(), 'data', 'posts.json');
   return readFile(filePath, 'utf-8')
     .then<Post[]>(JSON.parse)
     .then((posts) => posts.sort((a, b) => (a.date > b.date ? -1 : 1)));
-}
+});
+
 export async function getFeatuerdPosts() {
   const posts = await getAllPosts();
   return posts.filter((post) => post.featured);
